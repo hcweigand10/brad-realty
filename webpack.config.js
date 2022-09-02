c95de82614be603dc2d8a47beeaa9053d115382c
hcweigand10 <henryweigand10@gmail.com>
2022-09-02 16:41:07 -0700

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   output: {
-    path:path.resolve(__dirname, "dist"),
+    path:path.resolve(__dirname, "dist/js"),
   },
   mode: "production",
   module: {
