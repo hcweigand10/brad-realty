5efda8f3446b3d3b4ccafef01d4a188dd62a33c7
hcweigand10 <henryweigand10@gmail.com>
2022-09-05 13:58:57 -0700

    favicon

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "public", "index.html"),
+      favicon: "./src/images/compasslogo.jpeg"
     }),
   ],
 }
