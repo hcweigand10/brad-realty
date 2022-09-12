646265c687f02ccfb6d8d355419dd1ed5808c9a3
hcweigand10 <henryweigand10@gmail.com>
2022-09-12 11:30:44 -0700

    blog styling

--- src/components/blogItem/BlogPreview.js
+++ src/components/blogItem/BlogPreview.js
@@ -7,10 +7,10 @@ const BlogPreview = ({ blog }) => {
             {blog.isFeatured ? (
                 <div className='card blog-card'>
                     <div className='row'>
-                        <div className='col-lg-6'>
-                            <img src={blog.thumbnail} alt="blog thumbnail" style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+                        <div className='col-lg-5'>
+                            <img src={blog.thumbnail} alt="blog thumbnail" style={{maxHeight: "250px", minWidth: "95%", objectFit: "cover"}} className="mx-auto"/>
                         </div>
-                        <div className='col-lg-6 my-auto'>
+                        <div className='col-lg-7 my-auto'>
                             <div className='p-3'>
                                 <h5 className='text-muted'>{blog.date}</h5>
                                 <h2 style={{fontWeight: "bold", color: "black"}}>{blog.title}</h2>
@@ -21,7 +21,7 @@ const BlogPreview = ({ blog }) => {
                     </div>
                 </div>
             ) : (
-                <div className='card blog-card' style={{height: "340px"}}>
+                <div className='card blog-card' style={{height: "100%"}}>
                     <img src={blog.thumbnail} alt="blog thumbnail" style={{maxWidth: "100%", height: "55%"}}/>
                     <div className='p-3'>
                         <h6 className='text-muted'>{blog.date}</h6>
